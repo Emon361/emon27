7190bdf3c3577f78bd2ef5c80b2f65ac33abdc7b
Emon361 <tariqulemon22@gmail.com>
2025-03-27 03:39:39 -0700

    first commit

---
+++ class.py
@@ -0,0 +1,538 @@
+# a = 0
+#  a = 0
+# if a < 20:
+# print()
+# print('hello emon')
+# a = input('text here')
+# print(a)
+# a = int(input('enter a number:'))
+# b = int(input('enter a number:'))
+# # print(a + b)
+# a = [1,2,7,2,1]
+# print(a.count(1))
+# class MyClass:
+#     a = 10
+#     b = 31
+#     def myfun(self):
+#         return 'hello'
+# x = MyClass()
+# print(x.myfun())
+# class A:
+#     a = 10
+#     b = 23
+# class B(A):
+#     c = 31
+#     d = 34
+# x = B()
+# print(x.a)
+# class MyClass:
+#     def __init__(self):
+#         print('hello world')
+#     a = 10
+#     b = 20
+#     c ='bangladesh'
+# x = MyClass()
+# print(x.c)
+# class Myself:
+#     def __init__(self,name,age):
+#         self.a = name
+#         self.b = age
+#     def greetings(self):
+#         return f'hello, my name is {self.a} and i am {self.b} years old.'
+# x = Myself('Emon',24)
+# print(x.greetings())
+# class Car:
+#     def move(self):
+#         print('drive')
+# class Plane:
+#     def move(self):
+#         print('fly')
+# class Boat:
+#     def move(self):
+#         print('sail')
+# car = Car()
+# plane = Plane()
+# boat = Boat()
+# print(car.move())
+# print(boat.move())
+# print(plane.move())
+# for i in (car,plane,boat):
+#     i.move()
+# class FileHandeling:
+#     def __init__(self,filename):
+#         self.file = open(filename,'w')
+#         print('your file is open')
+#     def write_data(self,data):
+#         self.file.write(data)
+#     def __del__(self):
+#         self.file.close()
+#         print('writtimg has been comlete.')
+# x = FileHandeling('my_file.text')
+# x.write_data('class shesh.')
+# b = input('Enter your country Name:')
+
+# a = f'I Love {b}'
+# # print(a.format(b))
+# print(a)
+
+# B = input('Enter your country name:')
+# a = f'I Love {B}'
+# print(a)
+# a = 'I Love \'Bangla\tdesh\''
+# print(a)
+
+# a = int(input('Enter a number:'))
+# b = int(input('Enter a number:'))
+# # print(a / b)
+# a = 'I Love Bangladesh'
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+# print(a.title())
+# print(a.index('B'))
+# print(a.replace('Bangladesh','Turkey'))
+# print(a.swapcase())
+# print(a.strip())
+# print(a.split())
+# print(len(a))
+# print(len(a.replace(" ","")))
+
+a = 35
+b = 16
+# print(a % b)
+# print(a // b)
+# print(a ** b)
+# a **= 2
+# a //= 2
+# print(a)
+# print(a == b)
+# print(a != b)
+# print(a <= b)
+# print(a > b and a == b)
+# print(a > b or a == b)
+# print(not(a > b and a == b))
+# print(a is b)
+# # print(a is not b)
+# b = 'I love bangladesh'
+# print('f' in b)
+# print('a' not in b)
+# 0 1 0 1
+# 0 0 1 1
+# ------------
+# 0 0 0 1
+# print(5 & 3)
+# 0 1 0 1
+# 0 0 1 1
+# --------
+# 0 1 1 1
+# print(5 | 3)
+# # print(~3)
+# a = 10
+# b = 23
+# if a == b:
+#     print('Hello Bangladesh')
+# elif a > b :
+#     print('i am bigger')
+# elif a < b :
+#     print('i am small')
+# else:
+#     print('not')
+# a = int(input( 'Enrer a students result:'))
+# if a > 100 or a < 0 :
+#     print('invalid number')
+# else :
+#     if a >= 80:
+#         print('A+')
+#     elif a >= 70 :
+#         print('A')
+#     elif a >= 60 :
+#         print('A-')
+#     elif a >= 40 :
+#         print('B')
+#     elif a < 40 :
+#         print('F')
+# a = int(input('Enter a Number:'))
+# if a / 1 and a / a and a / 2 and a / 3 :
+#     print('coPrime')
+# else :
+#     print('prime')
+# a = int(input('Enter a number:'))
+# b = int(input('Enter a number:'))
+# c = input('Enter S/A/M/D if you want Subtraction/Additon/Multiplication/Division:')
+# if c == 'S' :
+#     print(a - b)
+# elif c == 'A' :
+#     print(a + b)
+# elif c == 'M' :
+#     print(a * b)
+# elif c == "D" :
+#     print(a / b)
+# a = int(input('Enter a Number:'))
+# if a % 2 == 0 :
+#     print('Even')
+# else :
+#     print('Odd')
+
+# a = 10
+# while 17 > a :
+#     print('I love Cumilla')
+#     a += 1
+ 
+# a = 0
+# while a <= 10 :
+#     print('a')
+#     if a == 5 :
+#         break
+#     a += 1
+
+# a = 0
+# while a <= 10:
+#     a += 1
+#     if a == 5:
+#         continue
+#     print(a)
+# a = 'I love cumilla'
+# for myvar in a:
+#     print(myvar)
+# for myvar in a:
+#     if myvar == 'u':
+#         break
+# for myvar in a:
+#     if myvar == 'u':
+#         continue
+
+# for myvar in range(3,10):
+
+    # print(myvar)
+# for i in range(len(a)):
+# #     print(i , a[i])
+# for i in range(1 , 6):
+#     for j in range(i):
+#         print('*',end=' ')
+#     print()
+# a = 'I love cumilla'
+# for i in a:=
+#     print(i , end='')
+# a = int(input('enter a number:'))
+# b = 1
+
+# while b <= 10:
+#     print(a*b)
+#     b += 1
+# print('multiplication table:')
+# while(True):
+        
+#     Number = int(input('Enter a Number:'))
+
+#     for i in range(1,11):
+#         print(f"{Number} * {i} = {Number * i}")
+#     choice = input('Do u want continue (yes/no):')
+
+#     if choice == 'yes':
+#         continue
+#     elif choice == 'no':
+#         break
+#     elif choice == 'any other':
+#         print('invalid')
+
+# print('calculator')
+# print('for addition : add')
+# print('for subtraction: sub')
+# print('for multiplication: mul')
+# print('for division: div')
+# print('close: exit')
+# while(True):
+#     Opperation = input('enter the operation to performed:')
+#     if Opperation == 'exit':
+#         break
+#     a = int(input('Enter a Number:')) 
+#     b = int(input('Enter a Number:'))
+#     if Opperation == 'add':
+#         print(a + b)
+#     elif Opperation == 'sub':
+#         print(a - b)
+#     elif Opperation == 'mul':
+#         print(a * b)
+#     elif Opperation == 'div':
+#         print(a / b)
+#  r =
+# a = [12,'Bangladesh',True, 3,4,3,3]
+# print(a[1])
+# for i in a:
+#     print(i)
+# for i in range(len(a)):
+#     print(i , a[i])
+# print(a.index(12))
+# a.append('Turkey')
+# a.insert(2,'USA')
+# a.remove(3)
+# a.pop(3)
+# b = a.copy()
+# b.append('india')
+# print(a)
+# print(b)
+# print(a.count(3))
+# a.reverse()
+# print(a[4])
+# a = [d,y,e,w,u,i]
+# a.sort()
+# Country_name_show = []
+# while(True):
+    
+#     Country_name_input = input('Enter a country name:').title()
+#     if Country_name_input == 'exit'.title():
+#         break
+#     elif Country_name_input == 'pop'.title():
+#         Country_name_show.pop()
+#         print(Country_name_show)
+#     elif Country_name_input =='remove'.title():
+#         remove_item = input('Enter a country:').title()
+#         Country_name_show.remove(remove_item)
+#         print(Country_name_show)
+#     else:
+#         Country_name_show.append(Country_name_input)
+#         print(Country_name_show)
+
+# a = (2,'bd',6,87,True)
+# print(type(a))
+# print(a.count(6))
+# print(a.index(True))
+# b = list(a)
+# b.remove(2)
+# a = tuple(b)
+# print(a)
+# a = [2,7,8,5]
+# b = [3,'gft','iyu']
+# a.extend(b)
+# print(a)
+# a = [2,2,True,2,2,2]
+# for i in a:
+#     a.remove(2)
+# print(a)
+
+# a = {'Name':'Rahim', 'Age':40}
+# print(a['Name'])
+# print(a.keys())
+# print(a.values())
+# a.update({'Roll':'Executive'})
+# print(a)
+# print(a.get('Name'))
+# a.pop('Age')
+# # a.popitem()
+# b = a.copy()
+# b.popitem()
+# print(a)
+# print(b)
+# b = a.items()
+# print(b)
+# for i,j in a.items():
+#     print(f'{i}:{j}')
+
+# a = {'Bangladesh','India','Palistine','Singapur'}
+# b = {'Canada','USA','India'}
+# print(a)
+# for i in a:
+
+#     print(i)
+# a.add('Singapur')
+# b = a.copy()
+# print(b)
+# print(a)
+# a.discard('India')
+# a.remove('India')
+# a.pop()
+# print(a)
+# a.update(b)
+# c = a.difference(b)
+# c = a.intersection(b)
+# c = a.union(b)
+# c = a & b
+# c = a | b
+# print(c)
+
+# def myfunc():
+#     print('I love Bangladesh')
+# myfunc()
+# def myfunc(num1 , num2):
+#     print(num1 + num2)
+# myfunc(10 , 23)
+# def myfunc(*num):
+#     print(sum(num))
+# myfunc(1,5,6,3,8,9)
+# a = lambda x : x + 10
+# print(a(5))
+# a = lambda: 'Hello Bangladesh'
+# print(a())
+# x = int(input('Enter a number:'))
+
+# a = 20
+# b = 32
+# if a < b:
+#     print('a is smaller then b')
+# else:
+#     print('a is bigger then b')
+# x = 'a is smaller then b' if a < b else 'a is bigger then b'
+# print(x)
+# print('a is bigger then b') if a < b else 'a is bigger then b'
+
+# x = int(input('Enter a number:'))
+
+# a = lambda z: 'even' if x % 2 == 0 else 'odd'
+# print(a(x))
+
+# def factorial(n):
+#     if  n== 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+    
+ 
+# print(factorial(8))
+
+# a = [i ** 2 for i in range(5)]
+# print(a)
+
+# a = open('C:/Intel/myfile109/my_file.text', 'r')
+# print(a.read())
+
+# a = open('C:/Intel/myfile109/my_file.text', 'a')
+# a.write('I Love Cumilla Cantonment')
+# a = open('C:/Intel/myfile109/my_file.text', 'r')
+# print(a.read())
+# import os
+# old_file = 'my_file.text'
+# new_file = 'test.txt'
+# os.rename('old_file','new_file')
+# os.rename('my_file.text','text_file.txt')
+# if os.path.exists('test_file.txt_2'):
+#     os.remove('text_file.txt')
+# else:
+#     print('File dosenot exist')
+
+# try:
+#     pass
+# except:
+#     pass
+
+# try:
+#     a = open('C:/Intel/myfile109/my_file.text', 'x')
+#     a.write('I Love Cumilla Cantonment')
+#     a = open('C:/Intel/myfile109/my_file.text', 'r')
+#     print(a.read())
+# except:
+#     print('file not exist')
+# finally:
+#     print('hgfy')
+# a = [2,2,True,2,2,2]
+# for i in a:
+#     a.remove(2)
+# print(a)
+
+# try:
+#     a = open('C:/Intel/myfile109/my_file.text', 'x')
+#     a.write('I Love Cumilla Cantonment')
+#     a = open('C:/Intel/myfile109/my_file.text', 'r')
+#     print(a.read())
+# except Exception as e:
+#     print(e)
+# finally:
+#     print('hgfy')
+
+
+# import my_newproduct
+# print(my_newproduct.f)
+# from my_newproduct import f
+# print(f)
+
+# import datetime
+# a = datetime.datetime.now()
+# print(a)
+# print(a.strftime('%A'))
+# print(a.strftime('%Y'))
+# print(a.strftime('%I : %M : %S %p'))
+
+# import pandas as pd
+
+# data = {
+#     'Name' : ['Emon','Prince','Shadin','Sumon'],
+#     'Age' : [24,25,23,24],
+#     'Area' : ['Uttara','Badda','Mohakhali','Banasree']
+
+# }
+# df = pd.DataFrame(data)
+# # print(df)
+# df.to_csv('output.csv', index=False)
+
+# class Myclass:
+#    a = 10
+#    b = 21
+#    def myfun(self):
+#       return 'HEllo'
+# x = Myclass()
+# print(x.myfun())
+
+# class A :
+#     a = 10
+#     b = 20
+# class B(A):
+#     c = 21
+#     d = 32
+# x = B()
+# print(x.a)
+
+# class Myclass:
+#     def __init__(self):
+#         print("Hello world")
+#     a = 20
+#     b = 42
+#     c = "I Love Bd"
+# x = Myclass()
+# print(x.a)
+
+# class Myclass:
+#     def myfun(self,num1,num2):
+#         print(num1+num2)
+# x = Myclass()
+# x.myfun(12,23)
+
+# class Person:
+#     def __init__(self,Name,Age):
+#         self.a = Name
+#         self.b = Age
+#     def greatings(self):
+#         return f'Hello I am {self.a} and I am {self.b} yrs old'
+# x = Person("Rahim",32)
+# print(x.greatings())
+
+# class car:
+#     def move(self):
+#         print('drive')
+# class plane:
+#     def move(self):
+#         print('fly')
+
+# class boat:
+#     def move(self):
+#         print('sail')
+# car = car()
+# plane = plane()
+# boat = boat()
+# # print(car.move())
+# # print(plane.move())
+# # print(boat.move())
+# for i in (car,plane,boat):
+#     i.move()
+
+
+# class Filehandeling():
+#     def __init__(self,filename):
+#         self.file = open(filename,'w')
+#         print('Your file is open')
+#     def write_data(self,data):
+#         self.file.write(data)
+#     def __del__(self):
+#         self.file.close()
+#         print('writting has been complete')
+# x = Filehandeling('myfile.txt')
+# x.write_data('Class shesh')
+
